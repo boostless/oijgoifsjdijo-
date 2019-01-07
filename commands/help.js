e2009cda02b3624b022f05bef25cc60dcfccfe8a
boostless <46201063+boostless@users.noreply.github.com>
2019-01-07 15:35:18 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args)=>{
   let helpembed = new Discord.RichEmbed()
   .setDescription("~Pagalba~")
   .setColor("#4f80ff")
-  .addField("Komandos", "$report @vardas kodel \n $serverinfo \n $botinfo \n $kick @vardas kodel \n $ban @vardas kodel \n $tempmute @vardas s/m/h/d \n $clear kiekis \n $level \n $ping \n $coins \n $set @vardas kiekis \n $prefix \n $flip h;herbas/s;skaicius suma");
+  .addField("Komandos", "$report @vardas kodel \n $serverinfo \n $botinfo \n $kick @vardas kodel \n $ban @vardas kodel \n $tempmute @vardas s/m/h/d \n $clear kiekis \n $level \n $ping \n $coins \n $set @vardas kiekis \n $prefix \n $flip h;herbas/s;skaicius suma \n $gamble j;juoda/r;raudona/z;zalia");
   message.delete().catch(O_o=>{});
   message.author.send(helpembed);
 }
