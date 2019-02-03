23f4d15908a691b893b495c8991b8f6cbad03acb
boostless <46201063+boostless@users.noreply.github.com>
2019-02-03 12:10:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ bot.on("message", async message => {
 
   };
   //Chekinam ar play komanda yra parasyta ne bot chaneli
-  if(msg.includes("!PLAY") || msg.includes("!JOIN") || msg.includes("!CLEAR"))
+  if(msg.includes("!PLAY") || msg.includes("!JOIN") || msg.includes("!CLEAR") || msg.includes("!SKIP"))
   {
     if(message.channel.id !== "527557990241533972")
     {
